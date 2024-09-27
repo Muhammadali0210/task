cd8f7a1f8c62c2e2dcc23b19f2a6fe78bd4b7e54
Muhammadali0210 <theprogramer0210@gmail.com>
2024-09-27 13:08:46 +0500

    First commit

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/Ali/Desktop/task/task/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///C:/Users/Ali/Desktop/task/task/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
